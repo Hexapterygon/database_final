295f4766da04c77c94237e212c964275fd563918
Nathan Anderle <anderle.nathan8@gmail.com>
2016-03-18 12:26:05 -0400

    Started building tables

--- CIS353-project.sql
+++ CIS353-project.sql
@@ -15,6 +15,75 @@ In the DDL, every IC must have a unique name; e.g. IC5, IC10, IC15, etc.
 */
 CREATE TABLE Teams
 (
+    teamID      INTEGER,
+    name        CHAR(15)    NOT NULL,
+    mascot      CHAR(15)    NOT NULL,
+    wins        INTEGER     NOT NULL,
+    losses      INTEGER     NOT NULL,
+    seed        INTEGER     NOT NULL,
+    region      CHAR(15)    NOT NULL,
+
+    --needs integrity constraints yet
+);
+-- ------------------------------------------------------
+CREATE TABLE Player
+(
+    playerID    INTEGER,
+    num         INTEGER     NOT NULL,
+    firstName   char(15)    NOT NULL,
+    lastName    char(15)    NOT NULL,
+    height      INTEGER     NOT NULL,
+    year        char(15)    NOT NULL,
+    teamID      INTEGER,     --not sure on nullness
+
+    --needs integrity constraints yet
+);
+--  -------------------------------------------------------
+CREATE TABLE Game
+(
+    gameID      INTEGER,
+    region      CHAR(15)    NOT NULL,
+    gDate       DATE,       NOT NULL,
+    winScore    INTEGER,    NOT NULL,
+    loseScore   INTEGER,    NOT NULL,
+    winner      INTEGER,    NOT NULL,
+    teamOne     INTEGER,    NOT NULL,
+    teamTwo     INTEGER,    NOT NULL,
+    nextGID     INTEGER,    NOT NULL,
+
+    --needs integrity constraints yet
+
+);
+--  -------------------------------------------------------
+CREATE TABLE Coach
+(
+    coachID     INTEGER,
+    firstName   CHAR(15),    NOT NULL,
+    lastName    CHAR(15),    NOT NULL,
+    wins        INTEGER,     NOT NULL,
+    losses      INTEGER,     NOT NULL,
+    team        INTEGER,     NOT NULL,
+    startYear   DECIMAL(4,0) NOT NULL,
+
+    --needs integrity constraints yet
+);
+--  -------------------------------------------------------
+CREATE TABLE Performance
+(
+    playerID    INTEGER,
+    gameID      INTEGER,
+    points      INTEGER,    NOT NULL,
+    rebounds    INTEGER,    NOT NULL,
+    assists     INTEGER,    NOT NULL,
+
+    --needs integrity constraints yet
+
+);
+--  -------------------------------------------------------
+CREATE TABLE PreviouslyCoached
+(
+
+
 
 -- 
 SET FEEDBACK OFF 
